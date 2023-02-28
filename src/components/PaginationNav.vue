c2de5522beb0e8a519463f29431f4f8bc60eb0ae
teschiopol <paolo.concolato@eniac.it>
2023-02-28 16:01:02 +0100

    chore(components): clean pastebin

--- src/components/PaginationNav.vue
+++ src/components/PaginationNav.vue
@@ -3,11 +3,6 @@
     <div style="width: 30%; text-align: center; margin: auto 0">
       <ButtonStandard label="Create 🗒" @click="toggleModal"/>
     </div>
-  <!-- TODO: search autocomplete over 3 letters
-    <div style="width: 30%; text-align: start; margin: auto 0">
-      <ButtonStandard label="Search 🔎" />
-    </div>
-    -->
   </div>
   <ModalComponent @close="toggleModal" :isOpen="isOpen" @save="save" >
     <div class="modal-content">
@@ -17,11 +12,6 @@
           <input id="siteName" class="input" type="text" placeholder="" autocomplete="off" v-model="siteName" />
           <div class="cut"></div>
           <label for="siteName" class="placeholder">Name</label>
-        </div>
-        <div class="input-container ic2">
-          <input id="hostLink" class="input" type="text" placeholder="" autocomplete="off" v-model="hostLink" />
-          <div class="cut"></div>
-          <label for="hostLink" class="placeholder">Host Link</label>
         </div>
       </form>
     </div>
@@ -40,7 +30,6 @@
       const hideButton = ref(false);
 
       const cleanFields = () => {
-        hostLink.value = '';
         siteName.value = '';
       };
 
@@ -50,43 +39,18 @@
         isOpen.value = !isOpen.value;
       };
 
-      const check = (value) => {
-        return value !== '';
-      };
-
       const save = () => {
-        if(siteName.value !== '' && check(hostLink.value) ){
+        if(siteName.value !== ''){
           let body = [];
-          body['hostLink'] = hostLink.value;
           body['siteName'] = siteName.value;
           hideButton.value = true;
-          fetch( 'https://neterprise.eniac.it/api/345bues/neterprise/server_handler.jsons', {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json;charset=utf-8',
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Headers' : 'Content-Type, Authorization, X-Requested-With'
-
-            },
-            body: JSON.stringify( body )
-          })
-          .then( function( response ){
-            if( response.status !== 200 ){
-              console.log(response.status);
-            }else{
-              console.log('Insert');
-              window.location.reload();
-            }
-          })
         }
         cleanFields();
         isOpen.value = !isOpen.value;
       };
 
-      let hostLink = ref('');
       let siteName = ref('');
-      return {isOpen, toggleModal, save, hostLink, siteName, hideButton};
+      return {isOpen, toggleModal, save, siteName, hideButton};
     }
   }
 </script>
