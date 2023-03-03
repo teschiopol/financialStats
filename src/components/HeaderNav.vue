156057608abfac01e683746edd4714ba97e114c4
teschiopol <paolo.concolato@eniac.it>
2023-03-03 15:46:44 +0100

    WIP(components): dark mode

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -4,10 +4,14 @@
     <div>
       <a class="basic" href="https://www.unicorne.it/" target="_blank">Develop by Unicorne 🦄</a>
     </div>
+    <div>
+      <button class="button" @click="toggle(!isDarkMode)">{{isDarkModeLabel}}</button>
+    </div>
   </div>
 </template>
 
 <script>
+  import {onMounted, ref} from "vue";
   export default {
     name: "HeaderNav",
     props: {
@@ -15,6 +19,23 @@
         type: Boolean,
         default: true
       }
+    },
+    setup(){
+      const isDarkMode = ref(false);
+      const isDarkModeLabel = ref("Light");
+  // TODO: dark mode
+      const toggle = (payload) => {
+        isDarkMode.value = payload;
+        isDarkModeLabel.value = payload ? 'Dark' : 'Light';
+        //document.documentElement.className = payload;
+        localStorage.setItem('theme', payload);
+      };
+
+      onMounted(() => {
+        toggle(JSON.parse(localStorage.getItem('theme')));
+      });
+
+      return{isDarkMode, isDarkModeLabel, toggle}
     }
   }
 </script>
@@ -33,5 +54,16 @@
   }
   .informative{
     height: 2em;
+  }
+  .button{
+    background-color: var(--aliceblue);
+    border: none;
+    color: var(--darkcyan);
+    padding: 2px 10px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
   }
 </style>
