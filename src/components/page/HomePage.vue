5727c9644d4e9cd2055dc214dbd4df17473e6957
teschiopol <paolo.concolato@eniac.it>
2023-03-03 01:36:00 +0100

    feat(standard): setup chart

--- src/components/page/HomePage.vue
+++ src/components/page/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <HeaderNav/>
-  <div style="width:50%; display:inline-block;">
+  <div style="width:35%; display:inline-block;">
     <CardStandard :relevance="relevance"/>
   </div>
-  <div style="width:50%; display:inline-block;">
-
+  <div style="width:65%; display:inline-block;">
+    <ChartStandard :configuration="config"/>
   </div>
 </template>
 
@@ -13,26 +13,59 @@
   // TODO: sign up with Google
   import HeaderNav from "@/components/HeaderNav";
   import CardStandard from "@/components/standard/CardStandard";
-  import {ref} from "vue";
+  import {onMounted, ref} from "vue";
+  import ChartStandard from "@/components/standard/ChartStandard";
   export default {
     name: "HomeBase",
     title: "Home",
     components: {
+      ChartStandard,
       CardStandard,
       HeaderNav
     },
     setup(){
-      const relevance = ref([
-        {id:"Essentials", value:40}, {id:"Useful",value:39}, {id:"Useless",value:14}, {id:"Regret",value:7}
-      ]);
-      return {relevance};
-    },
-    mounted() {
+
   // TODO: api get cookie token auth
       let user = localStorage.getItem('user-info');
       if(!user){
         this.$router.push({name:"Login"});
       }
+
+      const relevance = ref([
+        {id:"Essentials", value:40}, {id:"Useful",value:39}, {id:"Useless",value:14}, {id:"Regret",value:7}
+      ]);
+
+      const config = {
+        type: 'bar',
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3, 18, 32, 21, 21, 6, 33],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      };
+
+      onMounted(() => {
+        const ctx = document.getElementById('myChart');
+        ctx.style.maxHeight = '400px';
+      })
+
+      return {relevance, config};
     }
   }
 </script>
+
+<style scoped>
+  .chartContainer{
+    padding: 0 50px 0 0;
+  }
+</style>
---
+++ src/components/standard/ChartStandard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="chartContainer">
+    <canvas id="myChart"></canvas>
+  </div>
+
+</template>
+
+<script>
+  import Chart from "chart.js/auto";
+  import { onMounted } from 'vue'
+  export default {
+    name: "ChartStandard",
+    props: ["configuration", "customStyle"],
+    setup(props) {
+
+      onMounted(() => {
+        const ctx = document.getElementById('myChart');
+        const myChart = new Chart(ctx, props.configuration);
+        myChart;
+      })
+
+    }
+  }
+</script>
