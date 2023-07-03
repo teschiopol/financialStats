8702a70dc62a2574401c4b04b52ebac4bddc88e8
teschiopol <paolo.concolato@eniac.it>
2023-07-03 08:48:53 +0200

    WIP(page): setting language

--- src/components/page/SettingPage.vue
+++ src/components/page/SettingPage.vue
@@ -3,9 +3,9 @@
   <SidebarStandard/>
   <h2 style="text-align: center">Setting</h2>
   <div class="container">
-    <div class="row">Delete all your data forever!</div>
+    <div class="row">Change Language</div>
     <div style="margin: auto">
-      <ButtonStandard label="Clear Data" @click="clear()"/>
+      <ButtonStandard :label="lang_label" @click="changeLanguage()"/>
     </div>
     <hr style="border-top: 0 solid #bbb;">
     <hr style="border-top: 0 solid #bbb;">
@@ -13,45 +13,80 @@
     <div style="margin: auto">
       <ButtonStandard label="Download Data" @click="save()"/>
     </div>
+    <hr style="border-top: 0 solid #bbb;">
+    <hr style="border-top: 0 solid #bbb;">
+    <div class="row">Delete all your data forever!</div>
+    <div style="margin: auto">
+      <ButtonStandard label="Clear Data" @click="toggleModal"/>
+    </div>
   </div>
 
+  <ModalComponent @close="toggleModal" :isOpen="isOpen" @del="clear" operation="D">
+    <div class="modal-content">
+      <h2>Clear Data</h2>
+      <div>
+        Sei sicuro di voler eliminare tutti i dati?
+      </div>
+    </div>
+  </ModalComponent>
 
 </template>
 
 <script>
-  // TODO: language change
-  // TODO: confirm on click
-  import router from "@/routers";
-  import HeaderNav from "@/components/HeaderNav";
-  import SidebarStandard from "@/components/standard/SidebarStandard";
-  import ButtonStandard from "@/components/standard/ButtonStandard";
-  export default {
-    name: "SettingPage",
-    components: {ButtonStandard, SidebarStandard, HeaderNav},
-    title: "Settings",
-    setup() {
-      let user = localStorage.getItem('user-info');
-      if (!user) {
-        router.push({name: "Login"});
-      }
+import router from "@/routers";
+import HeaderNav from "@/components/HeaderNav";
+import SidebarStandard from "@/components/standard/SidebarStandard";
+import ButtonStandard from "@/components/standard/ButtonStandard";
+import {ref} from "vue";
+import ModalComponent from "@/components/standard/ModalStandard";
 
-      const clear = () => {
-        localStorage.setItem('list', null);
-        console.log(localStorage.getItem('list'));
-      }
+export default {
+  name: "SettingPage",
+  components: {ButtonStandard, SidebarStandard, HeaderNav, ModalComponent},
+  title: "Settings",
+  setup() {
 
-      const save = () => {
-        let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent((localStorage.getItem('list')));
-        let downloadAnchorNode = document.createElement('a');
-        downloadAnchorNode.setAttribute("href", dataStr);
-        downloadAnchorNode.setAttribute("download", "financialStat.json");
-        document.body.appendChild(downloadAnchorNode);
-        downloadAnchorNode.click();
-        downloadAnchorNode.remove();
-      }
-      return {clear, save};
+    const isOpen = ref(false);
+    const lang_label = ref('Current: IT 🇮🇹');
+
+    let user = localStorage.getItem('user-info');
+    if (!user) {
+      router.push({name: "Login"});
     }
+
+    const clear = () => {
+      localStorage.setItem('list', null);
+      console.log(localStorage.getItem('list'));
+      toggleModal();
+    };
+
+    const save = () => {
+      let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent((localStorage.getItem('list')));
+      let downloadAnchorNode = document.createElement('a');
+      downloadAnchorNode.setAttribute("href", dataStr);
+      downloadAnchorNode.setAttribute("download", "financialStat.json");
+      document.body.appendChild(downloadAnchorNode);
+      downloadAnchorNode.click();
+      downloadAnchorNode.remove();
+    };
+
+    const toggleModal = () => {
+      isOpen.value = !isOpen.value;
+    };
+
+    const changeLanguage = () => {
+      // TODO: language change
+      if (lang_label.value.includes('IT')) {
+        lang_label.value = 'Current: EN 🇬🇧';
+      } else {
+        lang_label.value = 'Current: IT 🇮🇹';
+      }
+    }
+
+    return {clear, save, toggleModal, changeLanguage, isOpen, lang_label};
   }
+
+}
 </script>
 
 <style scoped>
